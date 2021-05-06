ea4691fefc6f4f45ce8a56461ce6afd9d34b0205
Miki Watanabe <watami03@gmail.com>
2021-05-06 20:24:41 +0200

    add new templates and make screen transitions possible without data. Also delete unused templates.

--- app.rb
+++ app.rb
@@ -11,15 +11,19 @@ get '/form' do
   erb :form
 end
 
-get '/new' do
+post '/index' do
   redirect "/index?title=#{params[:title]}"
-  # redirect '/index'
-  # erb :index
 end
-# get '/markdown_template_page' do
-  # markdown :markdown_template_page
-# end
 
+get '/show_memo' do
+  @title = params['title']
+  @memo = params['memo']
+  erb :show_memo
+end
+
+get '/edit' do
+  erb :edit
+end
 # get '/erb_and_md_template_page' do
   # erb :erb_and_md_template_page, :locals => { :md => markdown(:erb_and_md_template_page) }
 # end
