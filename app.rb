f781f64d7351c66047eea3ce1cca635ac6b2d8b8
Miki Watanabe <watami03@gmail.com>
2021-05-16 16:49:12 +0200

    add: erb files and it is now able to view the contens of a memo by clicking on the file name on index.

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'erb'
+require 'json'
 
 get '/index' do
-  @title = params['title']
+  @filenames = Dir.glob("*.json", base:Dir.pwd)
+  
   erb :index
 end
 
@@ -12,18 +14,41 @@ get '/form' do
 end
 
 post '/index' do
-  redirect "/index?title=#{params[:title]}"
+  @filenames = Dir.glob("*.json", base:Dir.pwd)
+ 
+  title = params[:title]  
+  hash = {"id" => SecureRandom.uuid,"title" => title,"body" => params[:body]}
+  # hash["memo"] = []
+  # hash["memo"].append({"id": SecureRandom.uuid, "title": title, "body": params[:body]})
+  
+  # File.open("#{title}.json", 'w') do |f|
+  #   json_format = JSON.generate(hash, f)
+  #   f.write(json_format)
+  # end  
+  # json_format = JSON.dump(hash, f)
+
+  File.open("#{title}.json", 'w') do |file|
+    file.puts JSON.generate(hash)
+  end
+  
+  erb :index
 end
 
-get '/show_memo' do
+get '/show_memo/:title' do
   @title = params['title']
-  @memo = params['memo']
+  
+  json = File.read("./#{@title}.json")
+  data_hash = JSON.parse(json.to_json)
+  # File.open("./#{@title}.json") do |f|
+  #   hash = JSON.load(f)
+  # end
+  # data_hash = JSON.parse(hash.to_json)
+  hash = JSON.parse(data_hash)
+  @body = hash["body"]
+  
   erb :show_memo
 end
 
 get '/edit' do
   erb :edit
 end
-# get '/erb_and_md_template_page' do
-  # erb :erb_and_md_template_page, :locals => { :md => markdown(:erb_and_md_template_page) }
-# end
