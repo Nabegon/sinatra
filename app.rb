74e720aae0337f5e908c72cd7ab1d8e2f806b0a4
Miki Watanabe <watami03@gmail.com>
2021-05-20 15:55:29 +0200

    update: rename and change routes.

--- app.rb
+++ app.rb
@@ -13,39 +13,39 @@ not_found do
   'not found'
 end
 
-get '/index' do
+get '/memos' do
   files = Dir.glob("*.json", base:Dir.pwd)
   file_datas = files.map { |file| File.read(file) }
-  @hash = file_datas.map { |line| JSON.parse(line) }
+  @hash = file_datas.map { |data| JSON.parse(data) }
    
   erb :index
 end
 
-get '/form' do
+get '/memos/new' do
   erb :form
 end
 
-post '/index' do
+post '/memos' do
   id =  SecureRandom.uuid
   hash = {id: id, title: params[:title], body: params[:body]}
   File.open("#{id}.json", 'w') { |file| file.puts JSON.generate(hash)}
-  redirect '/index'
+  
+  redirect to "/memos/#{id}"
 end
 
 def file_name
   "./#{params[:id]}.json"
 end
 
-get '/show_memo/:id' do
+get '/memos/:id' do
   json = File.read(file_name)
   data_hash = JSON.parse(json.to_json)
   @hash = JSON.parse(data_hash)
-  @id = params[:id]
 
   erb :show_memo
 end
 
-get '/edit/:id' do
+get '/memos/:id/edit' do
   json = File.read(file_name)
   data_hash = JSON.parse(json.to_json)
   @hash = JSON.parse(data_hash)
@@ -53,7 +53,7 @@ get '/edit/:id' do
   erb :edit
 end
 
-patch '/index/:id' do
+patch '/memos/:id' do
   title = params[:title]
   body = params[:body]
 
@@ -71,11 +71,11 @@ patch '/index/:id' do
     File.open(file_name, 'w') { |f| JSON.dump(hash, f) }
   end
 
-  redirect '/index'  
+  redirect to "/memos/#{params[:id]}"  
 end
 
-delete '/index/:id' do
+delete '/memos/:id' do
   File.delete(file_name)
   
-  redirect '/index'
+  redirect to "/memos"
 end
